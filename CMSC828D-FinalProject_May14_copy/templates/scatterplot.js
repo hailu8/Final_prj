43a1fc8366435c4030845fef9116747d71df4c4a
Hana A Hailu <kiyaa2010@gmail.com>
2021-05-14 19:15:36 -0400

    Update 7:15 pm

--- CMSC828D-FinalProject_May14_copy/templates/scatterplot.js
+++ CMSC828D-FinalProject_May14_copy/templates/scatterplot.js
@@ -7,14 +7,14 @@
 
 var Paper = 'all';
 var dt_from = "1970/05/04";
-var dt_to= "2015/12/01";
+var dt_to = "2015/12/01";
 // var dt_from;
 // var dt_to;
 
 $('.slider-time').html(dt_from);
 $('.slider-time2').html(dt_to);
-var min_val = Date.parse(dt_from)/1000;
-var max_val = Date.parse(dt_to)/1000;
+var min_val = Date.parse(dt_from) / 1000;
+var max_val = Date.parse(dt_to) / 1000;
 // var min_val;
 // var max_val;
 
@@ -22,25 +22,26 @@ function zeroPad(num, places) {
     var zero = places - num.toString().length + 1;
     return Array(+(zero > 0 && zero)).join("0") + num;
 }
+
 function formatDT(__dt) {
     var year = __dt.getFullYear();
-    var month = zeroPad(__dt.getMonth()+1, 2);
+    var month = zeroPad(__dt.getMonth() + 1, 2);
     var date = zeroPad(__dt.getDate(), 2);
     return year + '-' + month + '-' + date;
 };
 
 
 //default show 1970 - 2015
-let url_dif = new URL("http://localhost:8000/get-paper-date-range/all/1970-05-04/2015-12-01" );
+let url_dif = new URL("http://localhost:8000/get-paper-date-range/all/1970-05-04/2015-12-01");
 url_dif.search = new URLSearchParams().toString();
-        fetch(url_dif,{"credentials": "same-origin"})
-            .then(response => response.json())
-            .then(Datas => {
-                d1= Datas;
-                // console.log(d1);
-                UpdateScatterplot(d1,1970,2015);
+fetch(url_dif, { "credentials": "same-origin" })
+    .then(response => response.json())
+    .then(Datas => {
+        d1 = Datas;
+        // console.log(d1);
+        UpdateScatterplot(d1, 1970, 2015);
 
-});
+    });
 
 $("#slider-range").slider({
     range: true,
@@ -48,29 +49,29 @@ $("#slider-range").slider({
     max: max_val,
     step: 10,
     values: [min_val, max_val],
-    slide: function (e, ui) {
-        console.log(min_val,max_val);
-        dt_cur_from = new Date(ui.values[0]*1000); //.format("yyyy-mm-dd hh:ii:ss");
-        dt_cur_to = new Date(ui.values[1]*1000); //.format("yyyy-mm-dd hh:ii:ss");                
+    slide: function(e, ui) {
+        console.log(min_val, max_val);
+        dt_cur_from = new Date(ui.values[0] * 1000); //.format("yyyy-mm-dd hh:ii:ss");
+        dt_cur_to = new Date(ui.values[1] * 1000); //.format("yyyy-mm-dd hh:ii:ss");                
         $('.slider-time').html(formatDT(dt_cur_from));
         $('.slider-time2').html(formatDT(dt_cur_to));
         // var All = "all";
         console.log(Paper);
-        let url = new URL("http://localhost:8000/get-paper-date-range/"+ Paper  + "/" + formatDT(dt_cur_from) + "/" + formatDT(dt_cur_to));
+        let url = new URL("http://localhost:8000/get-paper-date-range/" + Paper + "/" + formatDT(dt_cur_from) + "/" + formatDT(dt_cur_to));
         url.search = new URLSearchParams().toString();
-        fetch(url,{"credentials": "same-origin"})
+        fetch(url, { "credentials": "same-origin" })
             .then(response => response.json())
             .then(Datas => {
-                d1= Datas;
+                d1 = Datas;
                 let mn = dt_cur_from.getFullYear();
                 let mx = dt_cur_to.getFullYear();
                 // console.log(mn);
                 // console.log(mx);
-                 UpdateScatterplot(d1,mn, mx);
-})
-        
+                UpdateScatterplot(d1, mn, mx);
+            })
 
-        }
+
+    }
 });
 
 
@@ -84,71 +85,64 @@ for (var i = 0; i < SelectPaperBtn.length; i++) {
         SelectPaper = this.value;
 
         if (SelectPaper === 'All') {
-            
+
             Paper = "all"
             console.log('All');
             console.log(Paper);
             time_from = document.getElementById('ranges');
             var spans = time_from.getElementsByTagName("span");
-            
+
             dt_cur_from = spans[0].innerHTML;
             dt_cur_to = spans[1].innerHTML;
-         
-            dt_cur_from = new Date (dt_cur_from);
-            dt_cur_to  = new Date (dt_cur_to);
-     
 
-            let url = new URL("http://localhost:8000/get-paper-date-range/"+ Paper  + "/" + formatDT(dt_cur_from) + "/" + formatDT(dt_cur_to));
+            dt_cur_from = new Date(dt_cur_from);
+            dt_cur_to = new Date(dt_cur_to);
+
+
+            let url = new URL("http://localhost:8000/get-paper-date-range/" + Paper + "/" + formatDT(dt_cur_from) + "/" + formatDT(dt_cur_to));
             url.search = new URLSearchParams().toString();
-            fetch(url,{"credentials": "same-origin"})
+            fetch(url, { "credentials": "same-origin" })
                 .then(response => response.json())
                 .then(Datas => {
-                    d1= Datas;
+                    d1 = Datas;
                     let mn = dt_cur_from.getFullYear();
-                    let mx = dt_cur_to.getFullYear(); 
-                    console.log(mn);
-                    console.log(mx);
-                    // let mn = 1970;
-                    // let mx = 2015;
-                    // console.log(d1);
-                    UpdateScatterplot(d1,mn, mx);
-            })
-        
-                    
-            
-        }
-          
-            
-        else if (SelectPaper === 'Black_Explosion') {
-           
+                    let mx = dt_cur_to.getFullYear();
+                    // console.log(mn);
+                    // console.log(mx);
+                    UpdateScatterplot(d1, mn, mx);
+                })
+
+
+
+        } else if (SelectPaper === 'Black_Explosion') {
+
             Paper = "black_explosion";
             console.log('Black_Explosion');
             console.log(Paper);
 
             time_from = document.getElementById('ranges');
             var spans = time_from.getElementsByTagName("span");
-            
+
             dt_cur_from = spans[0].innerHTML;
             dt_cur_to = spans[1].innerHTML;
-            
-            dt_cur_from = new Date (dt_cur_from);
-            dt_cur_to  = new Date (dt_cur_to);
 
-            let url = new URL("http://localhost:8000/get-paper-date-range/"+ Paper  + "/" + formatDT(dt_cur_from) + "/" + formatDT(dt_cur_to));
+            dt_cur_from = new Date(dt_cur_from);
+            dt_cur_to = new Date(dt_cur_to);
+
+            let url = new URL("http://localhost:8000/get-paper-date-range/" + Paper + "/" + formatDT(dt_cur_from) + "/" + formatDT(dt_cur_to));
             url.search = new URLSearchParams().toString();
-            fetch(url,{"credentials": "same-origin"})
+            fetch(url, { "credentials": "same-origin" })
                 .then(response => response.json())
                 .then(Datas => {
-                    d1= Datas;
+                    d1 = Datas;
                     //console.log(d1);
                     let mn = dt_cur_from.getFullYear();
-                    let mx = dt_cur_to.getFullYear(); 
-                    console.log(mn);
-                    console.log(mx);
-                UpdateScatterplot(d1,mn, mx);
-            })
-        
-            
+                    let mx = dt_cur_to.getFullYear();
+
+                    UpdateScatterplot(d1, mn, mx);
+                })
+
+
         } else {
             // var dt_from = "1983/09/01";
             // var dt_to= "2015/11/05";
@@ -160,115 +154,122 @@ for (var i = 0; i < SelectPaperBtn.length; i++) {
 
             time_from = document.getElementById('ranges');
             var spans = time_from.getElementsByTagName("span");
-            
+
             dt_cur_from = spans[0].innerHTML;
             dt_cur_to = spans[1].innerHTML;
-            
-            dt_cur_from = new Date (dt_cur_from);
-            dt_cur_to  = new Date (dt_cur_to);
+
+            dt_cur_from = new Date(dt_cur_from);
+            dt_cur_to = new Date(dt_cur_to);
 
             Paper = "Mitzpeh";
             console.log('Mitzpeh');
             console.log(Paper);
-            let url = new URL("http://localhost:8000/get-paper-date-range/"+ Paper  + "/" + formatDT(dt_cur_from) + "/" + formatDT(dt_cur_to));
+            let url = new URL("http://localhost:8000/get-paper-date-range/" + Paper + "/" + formatDT(dt_cur_from) + "/" + formatDT(dt_cur_to));
             // let url = new URL("http://localhost:8000/get-paper-date-range/" + Paper  + "/" +"2011-11-01/2015-11-05");
             url.search = new URLSearchParams().toString();
-            fetch(url,{"credentials": "same-origin"})
+            fetch(url, { "credentials": "same-origin" })
                 .then(response => response.json())
                 .then(Datas => {
-                    d1= Datas;
-                    //console.log(d1);
+                    d1 = Datas;
+                    // console.log(d1);
                     // let mn = 2011;
                     // let mx = 2015;
                     let mn = dt_cur_from.getFullYear();
-                    let mx = dt_cur_to.getFullYear(); 
-                    console.log(mn);
-                    console.log(mx);
-                    UpdateScatterplot(d1,mn, mx);
-            })
-        
-            
-                    }
-            });
-            
+                    let mx = dt_cur_to.getFullYear();
+
+
+                    // console.log(mn);
+                    // console.log(mx);
+                    UpdateScatterplot(d1, mn, mx);
+                })
+
+
         }
-    
-function UpdateScatterplot(Data, mn, mx){
+    });
+
+}
+
+function UpdateScatterplot(Data, mn, mx) {
     d3.selectAll("#circ").remove();
     d3.selectAll("#x_axis").remove();
+    d3.selectAll("#y_axis").remove();
     //  var w = 1290;
     //  var h = 1000;
-
-     var margin = {top: 40, right: 30, bottom: 100, left: 100},
-     w = 1290 - margin.left - margin.right,
-     h = 1000 - margin.top - margin.bottom;
+    // console.log(Data['year']);
+    // console.log(d3.min(Data.word_count));
+    // console.log(d3.max(Data.word_count));
+    var min_word = Math.min.apply(Math, Data.map(function(o) { return o.word_count; }))
+    var max_word = Math.max.apply(Math, Data.map(function(o) { return o.word_count; }))
+    var margin = { top: 20, right: 30, bottom: 100, left: 100 },
+        w = 1190 - margin.left - margin.right,
+        h = 1000 - margin.top - margin.bottom;
 
     var yearScale = d3.scaleLinear()
-    .domain([mn,mx]).range([90,w]);
+        .domain([mn, mx]).range([90, w]);
 
     var hrScale = d3.scaleLinear()
-    .domain([0,2600]).range([h,20]);
-    
+        .domain([min_word, max_word]).range([h, 30]);
+
     var xAxis = d3.axisBottom().scale(yearScale).ticks(20);
-		
-    var yAxis = d3.axisLeft().scale(hrScale).ticks(20);
+
+    var yAxis = d3.axisLeft().scale(hrScale).ticks(15);
 
     function scaleYear(year) {
         return yearScale(year);
     }
-    
+
     function scaleHomeruns(homeruns) {
         return hrScale(homeruns);
-    }   
+    }
     // **** Code for creating scales, axes and labels ****
-    
-    
+
+
     var svg = d3.select('svg');
     svg.append('g').attr('id', 'x_axis')
-        .attr('transform', 'translate(0,950)')
+        .attr('transform', 'translate(0,890)')
         .attr("width", w)
         .attr("height", h)
         .call(xAxis);
-        // .attr("width", width + margin.left + margin.right)
-        // .attr("height", height + margin.top + margin.bottom)
+    // .attr("width", width + margin.left + margin.right)
+    // .attr("height", height + margin.top + margin.bottom)
     svg.append('text')
         .attr('class', 'label')
-        .attr('transform','translate(360,1010)')
+        .attr('transform', 'translate(360,1010)')
         .text('Year');
-    
+
     svg.append('g').attr('id', 'y_axis')
         .attr('transform', 'translate(50,0)')
         .call(yAxis);
-    
+
     svg.append('text')
         .attr('class', 'label')
-        .attr('transform','translate(0,450) rotate(90)')
+        .attr('transform', 'translate(0,450) rotate(90)')
         .text('Word Count');
 
     groups = svg.selectAll("circle")
-    .data(Data)
-    .enter()
-    .append("g")
-    .attr('id', 'circ')
-    .attr('transform', function(d){
-        return 'translate('+scaleYear(d.year) +','+ scaleHomeruns(d.word_count) +')'
-    });
+        .data(Data)
+        .enter()
+        .append("g")
+        .attr('id', 'circ')
+        .attr('transform', function(d) {
+            return 'translate(' + scaleYear(d.year) + ',' + scaleHomeruns(d.word_count) + ')'
+        });
     // svg.call(tooltip);
     groups.append('circle')
-            .attr("cx", function(d) {
-                return d.yearScale;
-            })
-            .attr("cy", function(d) {
-                return d.hrScale;
-            })
-            .attr("r", 2.5)
-            .attr("stroke-width", 0.2)
-            .attr("stroke", "blue")
-            .on('click', function(d){
-                console.log(d.id);
-                let urls = new URL("http://localhost:8000/get-data/" + d.id);
-                urls.search = new URLSearchParams().toString();
-                fetch(urls,{"credentials": "same-origin"})
+        .attr("cx", function(d) {
+            return d.yearScale;
+        })
+        .attr("cy", function(d) {
+            return d.hrScale;
+        })
+        .attr("r", 1.5)
+        .attr("stroke-width", 0.2)
+        .attr("stroke", "blue")
+        .on('click', function(d) {
+            console.log(d.id);
+            let urls = new URL("http://localhost:8000/get-data/" + d.id);
+            urls.search = new URLSearchParams().toString();
+            fetch(urls, { "credentials": "same-origin" })
                 .then(response => response.json())
                 .then(Data2 => {
                     d2 = Data2
@@ -276,7 +277,18 @@ function UpdateScatterplot(Data, mn, mx){
                     $('.body_div').html(d2.body);
                     $('.paper_div').html(d2.paper);
                 })
-            });
+        });
+    groups.call(d3.zoom()
+        .extent([
+            [0, 0],
+            [w, h]
+        ])
+        .scaleExtent([1, 8])
+        .on("zoom", zoomed));
+
+    function zoomed({ transform }) {
+        groups.attr("transform", d => `translate(${transform.apply(d)})`);
+    }
 
 
 }
